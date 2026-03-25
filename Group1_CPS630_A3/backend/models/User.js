daea70d32763e4c105338c8211e6f0a529b4b1fd
csimpao <132604543+csimpao@users.noreply.github.com>
2026-03-25 00:59:30 -0400

    Delete Group1_CPS630_A3 directory

--- Group1_CPS630_A3/backend/models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-    username: { type: String, require: true },
-    password: { type: String, require: true }
-})
-
-export default mongoose.model('User', userSchema);
