b5cc5d05a89d0e51c516e374ee95ef6f75936e75
csimpao <132604543+csimpao@users.noreply.github.com>
2026-04-09 22:43:27 -0400

    added creatingauction functionality to button (#2)

--- frontend/src/components/dashboard/NewAuctionButton.tsx
+++ frontend/src/components/dashboard/NewAuctionButton.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+import CreateAuction from '../CreateAuction';
+
 export default function NewAuctionButton() {
+  const [isClicked, setIsClicked] = useState(false);
   return (
-    <button className="btn btn-outlined" type="button">
-      CREATE AUCTION
-    </button>
+    <>
+      <button className="btn btn-outlined" type="button" onClick={() => setIsClicked(true)}>
+        CREATE AUCTION
+      </button>
+      <CreateAuction isClicked={isClicked} onClose={() => setIsClicked(false)}/>
+    </>
   );
 }
